7f272aa8dc51e869e31571158f66d07490b293a6
themohitpapneja <43344470+themohitpapneja@users.noreply.github.com>
2020-01-28 19:36:38 +0530

    Update youtubesearch.py

--- youtubesearch.py
+++ youtubesearch.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 
 driver=webdriver.Chrome()
-driver.get('https://youtube.com')
+driver.get('https://youtube.com')  ##this specifies that the website it willl go to is youtube
 
 searchbox=driver.find_element_by_xpath('//*[@id="search"]')
-searchbox.send_keys('FunFury Space')
+searchbox.send_keys('FunFury Space')   ##Add the keywords you want to search
 
 searchButton=driver.find_element_by_xpath('//*[@id="search-icon-legacy"]/yt-icon')
 searchButton.click()
